60a3d0cb8229c1835bb70f842eb56a2c165fa893
Shota Shimazu <hornet.live.mf@gmail.com>
2018-11-07 02:16:31 +0900

    Initial Commit

---
+++ WebViewSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WebViewSample
+//
+//  Created by Shota Shimazu on 2018/11/07.
+//  Copyright © 2018 Shota Shimazu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
