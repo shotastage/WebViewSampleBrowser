d91ffa637f09a6e380a19db0af6893783240c156
Shota Shimazu <hornet.live.mf@gmail.com>
2018-11-07 02:33:03 +0900

    first commit

--- WebViewSample/ViewController.swift
+++ WebViewSample/ViewController.swift
@@ -7,14 +7,59 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKUIDelegate {
 
+    private var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        webView = WKWebView(frame: CGRect(x: 0,
+                                          y: 0,
+                                          width: self.view.bounds.size.width,
+                                          height: self.view.bounds.size.height))
+        
+        webView.allowsBackForwardNavigationGestures = true
+        
+        
+        let initialURL: String = "https://twitter.com/"
+        
+        let encodedUrlString = initialURL.addingPercentEncoding(withAllowedCharacters:NSCharacterSet.urlQueryAllowed)
+        
+        let url = URL(string: encodedUrlString!)
+        let request = NSURLRequest(url: url!)
+        
+        webView.load(request as URLRequest)
+        
+        
+        self.view.addSubview(webView)
+        provideURLBar()
+
     }
 
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
 
 }
 
+
+
+extension ViewController {
+    
+    fileprivate func provideURLBar() {
+        let bar: UIInputView!
+        
+        bar = UIInputView(frame: CGRect(
+            x: 0, y: 0,
+            width: self.view.bounds.width,
+            height: 20
+        ))
+        
+        self.view.bringSubviewToFront(bar)
+    }
+}
